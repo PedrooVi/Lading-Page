38e9948da035a9141ad24627d626d7de20fea7b1
Pedro Vinicius <139568403+PedrooVi@users.noreply.github.com>
2025-04-25 11:01:54 -0300

    Add files via upload

--- public/src/app.js
+++ public/src/app.js
@@ -1,5 +1,12 @@
 function navegar(url, seletor){
+    
     if(!url || !seletor) return
+
+    document.querySelectorAll('.section-principal').forEach(secao => {
+        secao.style.display = 'none'
+    })
+    
+
     const elemento = document.querySelector(seletor)
     fetch(url)
     .then(resp  => resp.text())
@@ -16,4 +23,18 @@ document.querySelectorAll('[nav-link]').forEach(link =>{
         e.preventDefault()
         navegar(url, seletorDestino)
     }
+})
+
+function navegacaoUnidades(id){
+    const elemento = document.getElementById(id)
+    if(elemento){
+        elemento.scrollIntoView({behavior: 'smooth'})
+    }
+}
+
+document.querySelectorAll('[nav-unidades]').forEach(link =>{
+    link.addEventListener('click', function (e){
+        e.preventDefault()
+        navegacaoUnidades('unidades')
+    })
 })
